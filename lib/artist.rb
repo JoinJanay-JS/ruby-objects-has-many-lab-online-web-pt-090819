33ba9eefd8d3e03caf77a02c30158a399df6271e
Janay Bailey <tjanay87@gmail.com>
2019-10-14 20:12:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,31 @@
-class Post
+class Artist
+  attr_accessor :name
 
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
   end
 
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
+  end
+
+  def self.song_count
+    @@song_count
   end
 end
